f92421c45b432761c2ecd5f148b0fdd405f4a092
demain_lee <lmm_work@163.com>
2023-07-13 16:12:58 +0800

    :white_check_mark: 更新测试

--- src/test/java/com/demain/dingtalk/DingTalkMessageTest.java
+++ src/test/java/com/demain/dingtalk/DingTalkMessageTest.java
@@ -1,6 +1,7 @@
 package com.demain.dingtalk;
 
 import com.demain.dingtalk.message.DingTalkLinkMessage;
+import com.demain.dingtalk.message.DingTalkTextMessage;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -10,12 +11,12 @@ import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 
 /**
- * 钉钉机器人链接消息测试
+ * 钉钉机器人消息测试
  *
  * @author demain_lee
- * @since 0.0.1
+ * @since 2023/7/13
  */
-public class DingTalkLinkMessageTest {
+public class DingTalkMessageTest {
 
 	DingTalkSend dingTalkSend;
 
@@ -23,11 +24,27 @@ public class DingTalkLinkMessageTest {
 	void before() throws NoSuchAlgorithmException, InvalidKeyException {
 		dingTalkSend = new DingTalkSend(
 				"https://oapi.dingtalk.com/robot/send?access_token=1baa692fa0c8a2400a9b05da0aa35fa47d320455769c702e632af8ba56bfd874");
-		dingTalkSend.setSecret("xxxxxx");
+		dingTalkSend.setSecret("xxx");
 	}
 
+	/**
+	 * 文本类型消息
+	 */
 	@Test
-	public void send() throws IOException {
+	public void dingTalkTEXTMessageSend() throws IOException {
+		DingTalkTextMessage dingTalkTextMessage = new DingTalkTextMessage();
+		dingTalkTextMessage.setContent("测试机器人监控报警消息通知999");
+		// dingTalkTextMessage.setAtMobiles("176xxxxx");
+		DingTalkResponse response = dingTalkSend.sendMessage(dingTalkTextMessage);
+		System.out.println(response.toString());
+		Assertions.assertTrue(response.isSuccess());
+	}
+
+	/**
+	 * 链接类型消息
+	 */
+	@Test
+	public void dingTalkLinkMessageSend() throws IOException {
 		DingTalkLinkMessage dingTalkLinkMessage = new DingTalkLinkMessage();
 		dingTalkLinkMessage.setText("测试机器人链接消息text");
 		dingTalkLinkMessage.setTitle("测试机器人链接消息title");
